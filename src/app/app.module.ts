e6558b39cba0ed592b46e2236b6eeef4a124629a
Asmaâ ELABID <asmaa.elabid@atos.net>
2023-10-12 11:07:25 +0100

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
+import { CustomerComponent } from './customer/customer.component';
+import { ProductComponent } from './product/product.component';
+import { OrderComponent } from './order/order.component';
+import { InnerHomeComponent } from './inner-home/inner-home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthentificationComponent,
+    CustomerComponent,
+    ProductComponent,
+    OrderComponent,
+    InnerHomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
